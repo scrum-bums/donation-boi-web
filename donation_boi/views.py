9d319836ee7b28b62ff794fff4f8f994ee6979d9
Evan Strat <evan10s@users.noreply.github.com>
2018-11-28 14:36:19 -0500

    Added store list view

--- donation_boi/views.py
+++ donation_boi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib.auth.mixins import UserPassesTestMixin,LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django import forms
 import json,datetime
 from django.contrib.auth.models import User, Group # need to import Group from https://stackoverflow.com/a/6288863/5434744
@@ -14,6 +14,10 @@ class StoreList(ListView):
     model = Store
     context_object_name = "stores"
 
+class StoreDetailView(DetailView):
+    model = Store
+    context_object_name = "store"
+    template_name = "donation_boi/store_detail.html"
 
 
 def logout_view(request):
