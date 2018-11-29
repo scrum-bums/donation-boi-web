9442312950f267afc89cd0fdee9f352747803003
Evan Strat <evan10s@users.noreply.github.com>
2018-11-29 02:21:07 -0500

    Add items to various store views, include the admin pages

--- donation_boi/admin.py
+++ donation_boi/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store, Item
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    fields = ["store", "name", "short_description", "long_description", "price", "category"]
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    number = 1
+
+class StoreAdmin(admin.ModelAdmin):
+    inlines = [ItemInline]
 
 admin.site.site_header = "Donation Boi Admin"
 
-admin.site.register(Store)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Item, ItemAdmin)
--- donation_boi/models.py
+++ donation_boi/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
+
 class Store(models.Model):
     name = models.CharField(max_length=60)
     address = models.CharField(max_length=200)
@@ -12,3 +13,25 @@ class Store(models.Model):
 
     def __str__(self):
         return "%s at %s" % (self.name, self.address)
+
+
+class Item(models.Model):
+    ITEM_TYPE_CHOICES = (('clothing', 'Clothing'),
+                        ('hat', 'Hat'),
+                        ('kitchen', 'Kitchen'),
+                        ('electronics', 'Electronics'),
+                        ('household', 'Household'),
+                        ('other', 'Other'),
+                         )
+
+    name = models.CharField(max_length=60)
+    short_description = models.CharField(max_length=250)
+    long_description = models.TextField()
+    price = models.DecimalField(decimal_places=2, default=0, max_digits=6)
+    category = models.CharField(choices=ITEM_TYPE_CHOICES,blank=False, max_length=120)
+    store = models.ForeignKey(Store, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.store)
